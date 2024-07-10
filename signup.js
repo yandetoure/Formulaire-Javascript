6a0586bde12d2e1b9f6a390a44a917f032bc7536
Girlhack007 <tourendeyeyande@gmail.com>
2024-07-10 16:47:07 +0000

    Rename du fichier signup.html en index.html

--- signup.js
+++ signup.js
@@ -16,32 +16,32 @@ button.addEventListener('click', function(event){
     let name = document.getElementById('name').value;
     if (name.length < 3 || name.length > 15){
         document.getElementById('nameError').innerHTML ="Le nom que vous avez saisi n'est pas valide, veuillez sair un nom entre 3 et 15 caractères";
-        valid =false;
+        // valid =false;
     }
 
     let adress = document.getElementById('adress').value;
     if (adress.length < 3 || adress.length > 25){
         document.getElementById('adressError').innerHTML ="Le nom que vous avez saisi n'est pas valide, veuillez sair un nom entre 3 et 15 caractères";
-        valid =false;
+        // valid =false;
     }
 
     let email = document.getElementById("email").value;
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
         document.getElementById('emailError').innerHTML ="L'adresse email que vous avez saisie n'est pas valide";
-        valid = false;
+        // valid = false;
     }
 
     let password = document.getElementById('password').value;
     if (password.length < 8){
         document.getElementById('passwordError').innerHTML = "Veuillez saisir un mot de passe d'au moins 8 caractères";
-        valid = false;
+        // valid = false;
     }
 
 
-    //Afficher les messages de succès
+    // // Afficher les messages de succès
     // if (valid){
-    //     document.getElementById(form-container).style.display = "none";
+    //     document.getElementById(soumissionForm).style.display = "none";
     //     doxument.getElementById('succesMessage').style.display = "block";
     // }
 
